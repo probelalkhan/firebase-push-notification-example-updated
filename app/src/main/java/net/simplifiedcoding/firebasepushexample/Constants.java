db525d8fd81ac5e093663435eb7c9ccabd757807
Belal Khan <probelalkhan@gmail.com>
2017-12-08 18:15:39 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/firebasepushexample/Constants.java
@@ -0,0 +1,12 @@
+package net.simplifiedcoding.firebasepushexample;
+
+/**
+ * Created by Belal on 12/8/2017.
+ */
+
+public class Constants {
+
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "Simplified Coding Notification";
+    public static final String CHANNEL_DESCRIPTION = "www.simplifiedcoding.net";
+}
---
+++ app/src/main/java/net/simplifiedcoding/firebasepushexample/MyFirebaseInstanceIdService.java
@@ -0,0 +1,30 @@
+package net.simplifiedcoding.firebasepushexample;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Belal on 12/8/2017.
+ */
+
+//the class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+
+    //this method will be called
+    //when the token is generated
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        //now we will have the token
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        //for now we are displaying the token in the log
+        //copy it as this method is called only when the new token is generated
+        //and usually new token is only generated when the app is reinstalled or the data is cleared
+        Log.d("MyRefreshedToken", token);
+    }
+}
